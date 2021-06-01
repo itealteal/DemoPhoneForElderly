501f86dfc5b0d952d66f8d498d6a08593e793a2c
20033824 <20033824@myrp.edu.sg>
2021-06-01 11:37:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demophoneforelderly/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.demophoneforelderly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button son;
+    Button daughter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        son = findViewById(R.id.btnSon);
+        daughter = findViewById(R.id.btnDaughter);
+
+        son.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+91234567));
+                startActivity(intentCall);
+            }
+        });
+
+        daughter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+98765432));
+                startActivity(intentCall);
+            }
+        });
+    }
+
+
+}
